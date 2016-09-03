f326eafcaf79230c9426843a5ebcd744f2f86561
Kurt <kurt.tholking@gmail.com>
2016-09-03 11:58:32 -0500

    Hello

---
+++ Eclipse/src/Eclipse/HelloEclipse.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Eclipse;
+
+/**
+ * @author Mr. T
+ * @class AP CSA
+ *
+ */
+public class HelloEclipse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World");
+	}
+
+}
